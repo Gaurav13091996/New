2833afe51ce53b468b2fc1ac1f6c040b0819e29f
Gaurav13091996 <117677117+Gaurav13091996@users.noreply.github.com>
2023-03-10 15:43:02 +0530

    Delete Icici directory

--- Icici/Iciciapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee_Details
-
-# Register your models here.
-
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['id','name','age','designation','doj']
-admin.site.register(Employee_Details,EmployeeAdmin)
--- Icici/Iciciapp/form.py
+++
@@ -1,9 +0,0 @@
-from .models import Employee_Details
-from django import forms
-from django.core import validators
-from django.forms import widgets
-
-class EmployeeRegistration(forms.ModelForm):
-    class Meta:
-        model=Employee_Details
-        fields=['id','name','age','designation','doj']
